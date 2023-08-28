9289feed4f987ec2ae106494f7bd8d42161fc1f0
cesarifuentes <fuentesivancesar@gmail.com>
2023-08-28 15:43:18 -0500

    Made new stylings to header text

--- portfolio_website/src/components/Sidebar.jsx
+++ portfolio_website/src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { socialMedias, resume } from "../constants";
+import globe from "../assets/globe.svg";
+import barcode from "../assets/barcode.png";
 
 const Sidebar = () => {
   return (
@@ -10,53 +12,58 @@ const Sidebar = () => {
 
       {/* Top */}
       <div className="flex flex-col gap-3 mt-5 lg:mt-0 pb-11 lg:pb-0">
-        <p className="uppercase">Welcome to my</p>
-        <h1>Front-End Developer Portfolio.</h1>
-        <p className="uppercase">
-          I build things on the web to deliver engaging, user-friendly online
-          experiences.
-        </p>
+        {/* Logo */}
+        <div className="flex align-middle">
+          <img className="w-[50px] h-[36px] " src={globe} alt="globe" />
+
+          <h4 className="p-1 ml-2 uppercase">
+            Hi my name is Cesar <br />
+            Welcome to my portfolio
+          </h4>
+        </div>
+
+        <h1 className="uppercase">
+          I create engaging, user-friendly online experiences.
+        </h1>
+
         <a href={resume} download>
-          <button class="bg-transparent border border-white py-2 px-4 rounded-full uppercase max-w-[100px]">
-            Resume
+          <button class="bg-transparent border-2 border-white py-2 px-5 rounded-full uppercase max-w-[300px]">
+            <h2 className="">Download Resume</h2>
           </button>
         </a>
+
+        <button class="bg-transparent border-2 border-white py-2 px-5 rounded-full uppercase max-w-[300px]">
+          <h2 className="">Email Me</h2>
+        </button>
       </div>
 
       {/* Bottom */}
       <div className="flex flex-col gap-3 mb-0 lg:w-[400px] lg:mb-20 lg:absolute lg:bottom-0 lg:left-0 ">
-        <div className="w-full line"></div>
-
         <div className="uppercase">
-          <h4>Location</h4>
-          <p className="">Dallas, Texas</p>
-        </div>
-        <div className="line"></div>
-
-        <div className="uppercase">
-          <h4>Contact</h4>
-          <p className="">fuentesivancesar@gmail.com</p>
-        </div>
-
-        <div className="line"></div>
-
-        <div className="uppercase">
-          <h4>Socials</h4>
           <div className="flex">
             {socialMedias.map((socialMedia, index) => (
               <div
                 key={socialMedia.id}
-                className={`${index === 0 ? "ml-0" : "ml-5"}`}
+                className={`${index === 0 ? "ml-0" : "ml-3"}`}
               >
                 <a href={socialMedia.link} target="_blank">
-                  <p className="uppercase">{socialMedia.name}</p>
+                  <img
+                    className="w-[25px] h-[25px]"
+                    src={socialMedia.icon}
+                    alt={socialMedia.name}
+                  />
                 </a>
               </div>
             ))}
           </div>
         </div>
 
-        <div className="line"></div>
+        <h3 className="uppercase">
+          Software Developer <br /> Based in Dallas Texas <br /> ©2023 All
+          Rights Reserved.{" "}
+        </h3>
+
+        <img className="w-[207px] h-[24px]" src={barcode} alt="globe" />
 
         {/* <h2 className="mb-1 text-white">Cesar Fuentes</h2>
         <h6 className="mb-3 text-white">Web Developer</h6> */}
